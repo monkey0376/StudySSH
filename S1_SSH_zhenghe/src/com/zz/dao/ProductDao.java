28c08ab50ca0dccbb6a90c9b4bc004f36fe1418f
monkey0376 <27955144+monkey0376@users.noreply.github.com>
2017-08-30 15:20:58 +0800

    直接引入HibernateDaoSuppor，在save方法内不用set方法就可直接调用

--- S1_SSH_zhenghe/src/com/zz/dao/ProductDao.java
+++ S1_SSH_zhenghe/src/com/zz/dao/ProductDao.java
@@ -1,11 +1,13 @@
 package com.zz.dao;
 
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
 import com.zz.domain.Product;
 
 /*
  * 商品管理类的DAO类
  */
-public class ProductDao {
+public class ProductDao extends HibernateDaoSupport{
 
 	/**
 	 * DAO 中的保存商品的方法
@@ -13,7 +15,7 @@ public class ProductDao {
 	 */
 	public void save(Product product) {
 		System.out.println("DAO中的方法执行了..");
-		
+		this.getHibernateTemplate().save(product);
 	}
 
 }
